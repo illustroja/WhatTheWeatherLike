48c3c7915f8b40c37b3eb80a54efaf8f91fc9510
BRYAN LEONG <bryan.leong.2020@scis.smu.edu.sg>
2022-05-22 16:15:14 +0800

    Added EditText for User to input their specific city. Added it to both portrait and landscape xml.

--- app/src/main/java/com/example/whattheweatherlike/MainActivity.kt
+++ app/src/main/java/com/example/whattheweatherlike/MainActivity.kt
@@ -4,9 +4,7 @@ package com.example.whattheweatherlike
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
@@ -15,28 +13,49 @@ import java.util.*
 import kotlin.random.Random
 import kotlin.math.roundToInt
 
-val country_code = "SG"
+var country_code = "SG"
 var locale1 = Locale("EN", country_code)
 class MainActivity : AppCompatActivity() {
-    val CITY: String = "singapore, sg"
-    val API:String = "125c6c233852de7920c9c5c910970c65"
+    var CITY: String = "singapore, sg"
+    var API:String = "125c6c233852de7920c9c5c910970c65"
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val updated_location:EditText=findViewById(R.id.input_location)
+        val update_location_btn:Button=findViewById(R.id.update_location_btn)
+
+
+        update_location_btn.setOnClickListener {
+            var new_location=updated_location.text.toString()
+            if(new_location.isEmpty()){
+                Toast.makeText(this,"Please include your current City",Toast.LENGTH_SHORT).show()
+            }else{
+                CITY = new_location
+                Toast.makeText(this,new_location,Toast.LENGTH_SHORT).show()
+            }
+            WeatherTask().execute()
+        }
+
 
 
         WeatherTask().execute()
     }
     inner class WeatherTask() : AsyncTask<String, Void, String>()
     {
-        override  fun onPreExecute() {
+        override fun onPreExecute() {
             super.onPreExecute()
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
             findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
             findViewById<TextView>(R.id.error_text).visibility = View.GONE
         }
         override fun doInBackground(vararg p0: String?): String?{
+
             var response:String? = try{
                 URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
             } catch (e: Exception) {
